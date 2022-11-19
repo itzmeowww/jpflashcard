9badb841b91bbe5acb0dbb0d2f9b01a0ca013fe1
itzmeowww <winthanasan@gmail.com>
2022-11-20 01:02:06 +0900

    feat: implemented search

--- phys.py
+++ phys.py
@@ -2,7 +2,7 @@
 import json
 
 # Opening JSON file
-f = open('physics.json')
+f = open('physics2.json')
 
 # returns JSON object as
 # a dictionary
@@ -10,9 +10,15 @@ data = json.load(f)
 
 # Iterating through the json
 # list
-for x in data:
-    for i in data[x]:
-        print(f"{i['kanji']}    {i['hiragana']}    {i['meaning']}")
+used = []
+with open('physic2.txt', 'w') as f:
+    for x in data:
+        for i in data[x]:
+            if i['kanji'] in used:
+                continue
+            used.append(i['kanji'])
+            print(i)
+            f.write(f"{i['kanji']}    {i['hiragana']}    {i['meaning']}\n")
 
 # Closing file
 f.close()
--- web/src/lib/GrammarList.svelte
+++ web/src/lib/GrammarList.svelte
@@ -5,51 +5,76 @@
   import links from "../data/jplang_grammar_link";
 
   let checked = {};
+  let query = "";
 </script>
 
 <section transition:fade>
-  <div class="flex flex-col w-full gap-4 items-center justify-center py-12">
+  <div
+    class="flex flex-col w-full gap-4 items-center justify-start py-16 min-h-screen"
+  >
     <button
       on:click={() => {
         selecting = true;
       }}
       class="fixed top-4 underline font-mono">go back</button
     >
+    <input
+      type="text"
+      placeholder="search"
+      class="border rounded-md pl-2 shadow-sm w-full sticky top-4 z-50"
+      bind:value={query}
+    />
     {#each Object.entries(lessons) as [key, lesson]}
-      <div class="border rounded-lg shadow p-4 flex flex-col gap-2 bg-white">
-        <h1
-          class="border border-black rounded-full w-6 h-6 flex items-center justify-center text-xs font-mono mx-auto"
-        >
-          {key}
-        </h1>
-        {#each lesson as grammar, idx}
-          <div class="flex gap-3 items-center justify-start w-64">
-            <h1 class="bg-black text-white rounded text-xs pr-1 w-8 text-right">
-              {grammar.split(" ")[0].split("-").splice(1).join(".")}
-            </h1>
-            <input
-              type="checkbox"
-              name=""
-              id={grammar.split(" ")[0]}
-              bind:checked={checked[grammar.split(" ")[0]]}
-              class="w-4"
-            />
-            <label
-              for={grammar.split(" ")[0]}
-              class={`w-full text-sm  break-all ${
-                checked[grammar.split(" ")[0]] && "line-through"
-              }`}
-            >
-              {grammar.split(" ").slice(1).join(" ")}
-            </label>
-            <a
-              target="_black"
-              href={`https://jplang.tufs.ac.jp/${links[key][idx]}`}
-              class="text-sm">↗️</a
-            >
-          </div>
-        {/each}
-      </div>
+      {#if lesson.filter((grammar) => grammar
+          .split(" ")
+          .slice(1)
+          .join(" ")
+          .toLowerCase()
+          .includes(query.toLowerCase())).length > 0}
+        <div class="border rounded-lg shadow p-4 flex flex-col gap-2 bg-white">
+          <h1
+            class="border border-black rounded-full w-6 h-6 flex items-center justify-center text-xs font-mono mx-auto"
+          >
+            {key}
+          </h1>
+          {#each lesson as grammar, idx}
+            {#if grammar
+              .split(" ")
+              .slice(1)
+              .join(" ")
+              .toLowerCase()
+              .includes(query.toLowerCase()) || query == ""}
+              <div class="flex gap-3 items-center justify-start w-64">
+                <h1
+                  class="bg-black text-white rounded text-xs pr-1 w-8 text-right"
+                >
+                  {grammar.split(" ")[0].split("-").splice(1).join(".")}
+                </h1>
+                <input
+                  type="checkbox"
+                  name=""
+                  id={grammar.split(" ")[0]}
+                  bind:checked={checked[grammar.split(" ")[0]]}
+                  class="w-4"
+                />
+                <label
+                  for={grammar.split(" ")[0]}
+                  class={`w-full text-sm  break-all ${
+                    checked[grammar.split(" ")[0]] && "line-through"
+                  }`}
+                >
+                  {grammar.split(" ").slice(1).join(" ")}
+                </label>
+                <a
+                  target="_black"
+                  href={`https://jplang.tufs.ac.jp/${links[key][idx]}`}
+                  class="text-sm">↗️</a
+                >
+              </div>
+            {/if}
+          {/each}
+        </div>
+      {/if}
     {/each}
   </div>
 </section>
